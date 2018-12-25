0fbc94fed7a0e0e0ea8f999341bb4c46081115e4
houxx <hou_xiangxiang@126.com>
2018-12-25 18:51:26 +0800

    解决中文名称下载问题

--- demo.py
+++ demo.py
@@ -3,7 +3,9 @@
 
 from flask import Flask
 from flask import request, send_from_directory
+from flask import make_response
 import codecs, os
+
 
 app = Flask(__name__)
 
@@ -35,7 +37,7 @@ def getContent():
 @app.route('/getFileName', methods=['GET', 'POST'])
 def getFileName():
     f = codecs.open(os.path.join(upload_path, temp_file_name), 'r', encoding='utf-8')
-    file_name = f.read()
+    file_name = f.read().encode("latin-1").decode('utf-8')
     f.close()
     return file_name
 
@@ -55,15 +57,16 @@ def uploadFile():
     # 需要从request对象读取表单内容：
     filedata = request.files.get('file')
     if filedata:
-        file_path = os.path.join(upload_path, filedata.filename)
+        file_name =  filedata.filename.encode("utf-8").decode('latin-1')
+        file_path = os.path.join(upload_path,file_name)
         f = codecs.open(os.path.join(upload_path, temp_file_name), 'w', encoding='utf-8')
-        f.write(filedata.filename)
+        f.write(file_name)
         f.close()
         try:
             filedata.save(file_path)  #  上传文件写入
         except IOError:
             return '上传文件失败'
-        return '上传文件成功, 文件名: {}'.format(file_path)
+        return u'上传文件成功, 文件名: {}'.format(file_path)
     else:
         return '上传文件失败'
 
@@ -73,4 +76,6 @@ def downloadFile():
     f = codecs.open(os.path.join(upload_path, temp_file_name), 'r', encoding='utf-8')
     file_name = f.read()
     f.close()
-    return send_from_directory(upload_path, file_name, as_attachment=True)  # 返回要下载的文件内容给客户端
+    response = make_response(send_from_directory(upload_path, file_name))
+    response.headers["Content-Disposition"] = u"attachment; filename={}".format(file_name)
+    return response # 返回要下载的文件内容给客户端
